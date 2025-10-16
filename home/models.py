2dd2d738c879de195ab071e6e526b59f4e0275ca
GurpritK <gurpritchana@hotmail.com>
2025-10-16 12:28:21 +0100

    Remove custom user model, add url path and update database settings

--- home/models.py
+++ home/models.py
@@ -1,17 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 from django.core.exceptions import ValidationError
-
-class User(AbstractUser):
-    role = models.CharField(
-        max_length=16,
-        choices=[("EMPLOYER", "Employer")],
-        default="EMPLOYER"
-    )
-
-    def __str__(self):
-        return self.username
+from django.contrib.auth.models import User
 
 
 class Job(models.Model):
