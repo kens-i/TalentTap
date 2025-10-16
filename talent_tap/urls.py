e3d36cd7fd7a4917528f877155bc12d192f53a3c
GurpritK <gurpritchana@hotmail.com>
2025-10-16 15:31:52 +0100

    Add edit, delete views and urls.py in app

--- talent_tap/urls.py
+++ talent_tap/urls.py
@@ -21,7 +21,5 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('', views.create_job, name='create_job'),
-    path('', views.job_detail, name='job_details'),
-    path('', views.job_list, name='job_list'),
+    path('jobs/', include('home.urls')),
 ]
